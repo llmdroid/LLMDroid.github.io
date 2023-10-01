962848faf40eb6e5b0814d86bf16a2e06f4b3864
gaolongxi <gaolongxi0123@gmail.com>
2023-10-01 13:48:51 +0800

    updated the index.html

--- static/js/index.js
+++ static/js/index.js
@@ -1,21 +1,36 @@
 window.HELP_IMPROVE_VIDEOJS = false;
 
-
 $(document).ready(function() {
-    // Check for click events on the navbar burger icon
+	var $carousel = $(".owl-carousel");
 
-    var options = {
-			slidesToScroll: 1,
-			slidesToShow: 1,
-			loop: true,
-			infinite: true,
-			autoplay: true,
-			autoplaySpeed: 5000,
+	$(".owl-carousel").owlCarousel({
+		items:3,
+		center:true,
+		loop:true,	
+		nav: true,  /* Enable navigation */
+		navText: ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+		responsive:{
+            0: {
+                items: 1
+            },
+            600: {
+                items: 3
+            },
+            1000: {
+                items: 3
+			}
+		},
+		onInitialized: updateCenter,
+        onTranslate: updateCenter,  // Update center on carousel transition
+	});
+
+    function updateCenter(event) {
+        var $items = $carousel.find('.owl-item');
+        $items.removeClass('center');
+        var $center = $carousel.find('.owl-item.active.center');
+        if ($center.length === 0) {  // If there's no active center item yet
+            $center = $carousel.find('.owl-item.active').eq(1);  // Assume the second active item is the center
+        }
+        $center.addClass('center');
     }
-
-		// Initialize all div with carousel class
-    var carousels = bulmaCarousel.attach('.carousel', options);
-	
-    bulmaSlider.attach();
-
-})
+});
